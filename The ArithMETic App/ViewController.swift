2e34b32288e8549b87965e4bc734434ce6e14f9d
Aditya <aditya@adityas-macbook-pro.local>
2019-02-14 18:33:08 -0600

    Initial Commit

---
+++ The ArithMETic App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The ArithMETic App
+//
+//  Created by Aditya on 14/02/19.
+//  Copyright © 2019 Aditya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
